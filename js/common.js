457e738f547010a7a3f707e3f4eb513ad21da524
Dmitriy Filippov <Amoralis.19@gmail.com>
2020-03-22 22:23:08 +0300

    1

--- js/common.js
+++ js/common.js
@@ -91,6 +91,7 @@ $(document).ready(function () {
     $('.header-top-mobile').on('click', function () { 
       jQuery('.header-center-wrap-center').toggleClass('menu-active');
       jQuery('.header-center-wrap-right').toggleClass('menu-active');
+      jQuery('.header-center-wrap-left ul').slideUp('menu-active');
       if($('.header-top-mobile').hasClass('header-top-mobile-active')) {
         jQuery('.header-top-mobile').toggleClass('menu-active-toggle-fixed');
 
